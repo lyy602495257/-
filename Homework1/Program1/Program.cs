042c993cdd0e3246efc972b3622a11379870c2bb
winwin <602495257@qq.com>
2018-09-12 18:44:46 +0800

    Homework1

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " ";
+            int n = 0;
+            int d = 0;
+            Console.Write("Please input an int:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.Write("Please input an int:");
+            s = Console.ReadLine();
+            d = Int32.Parse(s);
+            Console.WriteLine("Their product is :" + (n * d) );
+        }
+    }
+}
